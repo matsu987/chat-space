9c652970b32b32c591798e4d22dffe873f9c1801
chomatsuken-div <shogo.chomatsuken@di-v.co.jp>
2019-03-18 16:51:49 +0900

    deproy2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,9 @@
 # config valid for current version and patch releases of Capistrano
-set :linked_files, %w{ config/secrets.yml }
 
-lock "~> 3.11.0"
+lock '~> 3.11.0'
 
-set :application, "chat-space"
-set :repo_url, 'git@github.com:chomatsuken-div/chat-space.git'
+set :application, 'chat-space'
+set :repo_url,  'git@github.com:chomatsuken-div/chat-space.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -12,38 +11,18 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/key_pair.pem']
+                  keys: ['~/.ssh/key_pair2.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
-
-set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
-}
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
-
-  desc 'upload secrets.yml'
-  task :upload do
-    on roles(:app) do |host|
-      if test "[ ! -d #{shared_path}/config ]"
-        execute "mkdir -p #{shared_path}/config"
-      end
-      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
-    end
-  end
-  before :starting, 'deploy:upload'
-  after :finishing, 'deploy:cleanup'
 end
-
 
 
 # Default branch is :master
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '52.199.225.135', user: 'ec2-user', roles: %w{app db web}
+server '18.179.79.41', user: 'ec2-user', roles: %w{app db web}
 
 # server-based syntax
 # ======================
